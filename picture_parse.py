036c6110328671d4e950e6fcd0b302521a79bf87
Maxim Verzakov <mverzakov@crystalnix.com>
2016-10-08 14:36:30 +0600

    Count note length.

--- picture_parse.py
+++ picture_parse.py
@@ -1,12 +1,21 @@
-
 from PIL import Image
 import pprint
 
-def determine_octave_and_note(number):
-    octave = number / 64
-    note = (number % 64)*100 / 530
-    result = {"octave": octave, "note": note, "duration": 1}
-    return result
+
+def append_note(note_list, number):
+    note = {
+        "octave": number / 64,
+        "note": (number % 64) * 100 / 533,
+        "duration": 1
+    }
+
+    def same_note(x, y):
+        return x["octave"] == y["octave"] and x["note"] == y["note"]
+
+    if len(note_list) and same_note(note_list[-1], note):
+        note_list[-1]["duration"] += 1
+    else:
+        note_list.append(note)
 
 
 def parse_picture(path_image="test.jpeg", duration_step=5):
@@ -23,10 +32,12 @@ def parse_picture(path_image="test.jpeg", duration_step=5):
     #   {note:11, octave:0, duration:2}
     # ]
 
+    print 'started processing image'
     for i in pix_val:
-        R.append(determine_octave_and_note(i[0]))
-        G.append(determine_octave_and_note(i[1]))
-        B.append(determine_octave_and_note(i[2]))
+        append_note(R, i[0])
+        append_note(G, i[0])
+        append_note(B, i[0])
+    print 'finished processing image'
 
     return R, G, B
 
